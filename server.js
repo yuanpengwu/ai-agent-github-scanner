af2589927d75d5928b354e6606247e405eb36a05
yuanpengwu <yuanpeng.wu@gmail.com>
2026-05-23 23:37:21 -0700

    Upload server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const fs   = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end(fs.readFileSync(path.join(__dirname, 'index.html')));
+}).listen(PORT, '0.0.0.0', () => {
+  console.log('AI Agent Scanner listening on port ' + PORT);
+});
